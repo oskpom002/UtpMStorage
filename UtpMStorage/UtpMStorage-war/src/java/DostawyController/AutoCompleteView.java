3fef0e2d3aa35580b333923627806767e07f58e7
Oskar <Oskar@DESKTOP-AVMJ1QQ>
2018-01-20 03:14:02 +0100

    Added view for dostawy ZPO Project

--- UtpMStorage/UtpMStorage-war/src/java/DostawyController/AutoCompleteView.java
+++ UtpMStorage/UtpMStorage-war/src/java/DostawyController/AutoCompleteView.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DostawyController;
-
-/**
- *
- * @author Olek
- */
-
-import java.util.ArrayList;
+import DostawyController.Theme;
+import DostawyController.ThemeService;
+ import java.util.ArrayList;
 import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -18,19 +9,22 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.context.FacesContext;
  
 import org.primefaces.event.SelectEvent;
-import DostawyController.Theme;
-import DostawyController.ThemeService;
  
 @ManagedBean
 public class AutoCompleteView {
      
-    private String typ;
-    private String marka;
-    private String model;
-    private String imei;
-    private String magazyn;
-    private String data;
-
+    private String txt1;
+    private String txt2;
+    private String txt3;
+    private String txt4;
+    private String txt5;
+    private String txt6;
+    private String txt7;
+    private String txt8;
+    private Theme theme1;
+    private Theme theme2;
+    private Theme theme3;
+    private Theme theme4;
     private List<Theme> selectedThemes;
      
     @ManagedProperty("#{themeService}")
@@ -62,55 +56,102 @@ public class AutoCompleteView {
     public void onItemSelect(SelectEvent event) {
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Item Selected", event.getObject().toString()));
     }
-
-    public String getTyp() {
-        return typ;
+ 
+    public String getTxt1() {
+        return txt1;
     }
-
-    public void setTyp(String typ) {
-        this.typ = typ;
+ 
+    public void setTxt1(String txt1) {
+        this.txt1 = txt1;
     }
-
-    public String getMarka() {
-        return marka;
+ 
+    public String getTxt2() {
+        return txt2;
     }
-
-    public void setMarka(String marka) {
-        this.marka = marka;
+ 
+    public void setTxt2(String txt2) {
+        this.txt2 = txt2;
     }
-
-    public String getModel() {
-        return model;
+ 
+    public String getTxt3() {
+        return txt3;
     }
-
-    public void setModel(String model) {
-        this.model = model;
+ 
+    public void setTxt3(String txt3) {
+        this.txt3 = txt3;
     }
-
-    public String getImei() {
-        return imei;
+ 
+    public String getTxt4() {
+        return txt4;
     }
-
-    public void setImei(String imei) {
-        this.imei = imei;
+ 
+    public void setTxt4(String txt4) {
+        this.txt4 = txt4;
     }
-
-    public String getMagazyn() {
-        return magazyn;
+ 
+    public String getTxt5() {
+        return txt5;
     }
-
-    public void setMagazyn(String magazyn) {
-        this.magazyn = magazyn;
+ 
+    public void setTxt5(String txt5) {
+        this.txt5 = txt5;
     }
-
-    public String getData() {
-        return data;
+ 
+    public String getTxt6() {
+        return txt6;
     }
-
-    public void setData(String data) {
-        this.data = data;
+ 
+    public void setTxt6(String txt6) {
+        this.txt6 = txt6;
     }
-
+ 
+    public String getTxt7() {
+        return txt7;
+    }
+ 
+    public void setTxt7(String txt7) {
+        this.txt7 = txt7;
+    }
+ 
+    public String getTxt8() {
+        return txt8;
+    }
+ 
+    public void setTxt8(String txt8) {
+        this.txt8 = txt8;
+    }
+ 
+    public Theme getTheme1() {
+        return theme1;
+    }
+ 
+    public void setTheme1(Theme theme1) {
+        this.theme1 = theme1;
+    }
+ 
+    public Theme getTheme2() {
+        return theme2;
+    }
+ 
+    public void setTheme2(Theme theme2) {
+        this.theme2 = theme2;
+    }
+ 
+    public Theme getTheme3() {
+        return theme3;
+    }
+ 
+    public void setTheme3(Theme theme3) {
+        this.theme3 = theme3;
+    }
+ 
+    public Theme getTheme4() {
+        return theme3;
+    }
+ 
+    public void setTheme4(Theme theme4) {
+        this.theme4 = theme4;
+    }
  
     public List<Theme> getSelectedThemes() {
         return selectedThemes;
